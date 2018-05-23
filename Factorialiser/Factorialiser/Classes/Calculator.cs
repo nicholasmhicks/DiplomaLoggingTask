d814195fe81d5fee9048544664a58f7a8743d990
AUSTIN CASACELI GATTO <101622926@student.swin.edu.au>
2018-05-23 15:06:16 +1000

    a
    a

--- Factorialiser/Factorialiser/Classes/Calculator.cs
+++ Factorialiser/Factorialiser/Classes/Calculator.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Factorialiser.Classes
 {
     public static class Calculator
     {
+        private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
         /// <summary>
         /// Calculates and returns the factorial of the input integer
         /// </summary>
@@ -16,7 +18,6 @@ namespace Factorialiser.Classes
 
         public static int Factorial(int input)
         {
-            throw new NotImplementedException();
 
             // this method should:
 
@@ -32,6 +33,38 @@ namespace Factorialiser.Classes
             // if the number entered is >30 this should throw a NumberTooHighException.
             // if this occurs it should be logged (Debug) with the message as below:
             // "Calcuator.Factorial: input too high: 45"
+
+            _logger.Trace("calculating: " + input);
+
+            try
+            {
+                if (input < 1) throw new NumberTooLowException(input);
+                if (input > 30) throw new NumberTooHighException(input);
+
+                int answer = 1;
+
+                for (int i = input; i > 0; i--)
+                {
+                    answer *= i;
+                }
+
+                return answer;
+            }
+            catch (NumberTooHighException ex)
+            {
+                _logger.Trace("input too high: " + ex.Message);
+                throw ex;
+            }
+            catch (NumberTooLowException ex)
+            {
+                _logger.Trace("input too low: " + ex.Message);
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                _logger.Trace("unknown error message: " + ex.Message);
+                throw ex;
+            }
 
         }
     }
